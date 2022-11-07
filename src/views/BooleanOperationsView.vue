bee9029e7f8947a6e909c44a41cbbce080587c40
RobinBachus <robinbachus@gmail.com>
2022-11-07 02:12:36 +0100

    V0.0.2

---
+++ src/views/BooleanOperationsView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="main-container">
+    <h1>Plans:</h1>
+    <ul>
+      <li>Option for string or number (string only [==] and [!=])</li>
+      <li>
+        Check if expression is true ([&lt;], [>], [==], [&lt;=], [>=], [!=])
+      </li>
+      <li>Not flag [!] to reverse output</li>
+    </ul>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .main-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/views/RandomGeneratorView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="main-container">
+    <h1>Plans:</h1>
+    <ul>
+      <li>Get a given amount of random numbers between two values</li>
+    </ul>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .main-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+}
+</style>
